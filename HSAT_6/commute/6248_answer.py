008945f170c69fc3986558c56b1dae9318d1dcf4
jinclef <eve7777@hanyang.ac.kr>
2024-12-13 07:42:12 +0900

    출퇴근길 - answer - submit - success

--- HSAT_6/commute/6248_answer.py
+++ HSAT_6/commute/6248_answer.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 n, m = map(int, input().split()) # 정점, 간선 수
 
 adj = [[] for _ in range(n+1)] # 정점 번호 그대로 쓰기 위함
@@ -20,11 +23,14 @@ def dfs(nodeToVisit, adj, visit):
 
 # 기본 그래프
 fromS_visit = [0] * (n+1)
+fromS_visit[t] = 1
 dfs(s, adj, fromS_visit)
 fromT_visit = [0] * (n+1)
+fromT_visit[s] = 1
 dfs(t, adj, fromT_visit)
 
 # 반전된 그래프
+# 들어가면서 1 세팅하니까 얘는 따로 1 처리 해줄 필요도 없고 해서도 안됨.
 toS_visit = [0] * (n+1)
 dfs(s, adjR, toS_visit)
 toT_visit = [0] * (n+1)
@@ -34,4 +40,6 @@ count = 0
 for i in range(1, n+1):
     if fromS_visit[i] and fromT_visit[i] and toS_visit[i] and toT_visit[i]:
         count +=1
+    
+print(count-2) # 출근/퇴근 노드 제외
 
