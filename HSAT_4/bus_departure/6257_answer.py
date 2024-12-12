832b6e06dce4fa6fad1ed5e593effa249f10bc11
jinclef <eve7777@hanyang.ac.kr>
2024-12-13 06:44:52 +0900

    answer refactoring - more 1차원 배열

--- HSAT_4/bus_departure/6257_answer.py
+++ HSAT_4/bus_departure/6257_answer.py
@@ -1,14 +1,17 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-more = [[0 for _ in range(n)] for _ in range(n)] # i~k 사이의 값 개수
+# i 는 안바뀌기 때문에 굳이 more 이 2차원 배열일 필요 없음.
+more = [0 for _ in range(n)]
 total = 0
 for i in range(n):
+    more[i] = 0 # 재활용하니까 초기화 해줘야 함.
     for k in range(i+1, n): # 사실 i+2 부터 됨 중간에 j 있어서..
+        more[k] = more[k-1] # if, else에서 같은 동작
         if a[i] < a[k]: # 문제 조건 상 같을 수는 없음.
-            more[i][k] = more[i][k-1]+1
+            more[k] +=1
         else:
-            more[i][k] = more[i][k-1]
-            total += more[i][k]
+            more[k] = more[k-1]
+            total += more[k]
 
 print(total)
