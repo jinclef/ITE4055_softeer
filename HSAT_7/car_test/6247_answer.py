ccb12a928cc0df1c436b3b5003de89ed789476ba
jinclef <eve7777@hanyang.ac.kr>
2024-12-13 09:56:11 +0900

    자동차 테스트 - answer - set - success

--- HSAT_7/car_test/6247_answer.py
+++ HSAT_7/car_test/6247_answer.py
@@ -3,10 +3,11 @@ import bisect
 n, q = map(int, input().split())
 mileage = list(map(int, input().split()))
 mileage.sort()
+setMileage = set(mileage) # hash -> 상수시간에 탐색
 
 for _ in range(q):
     m = int(input())
-    if m not in mileage:
+    if m not in setMileage: # list 에서 있는지 확인하려면 리스트 전체를 다 뒤져야 하는데 너무 길어요 (5만*20만..> 10억)
         print(0)
     else:
         idx = bisect.bisect_left(mileage, m)
