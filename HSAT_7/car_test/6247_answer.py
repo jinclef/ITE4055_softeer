07570a7233f2643ef8a06928b3d5f6028be4f837
jinclef <eve7777@hanyang.ac.kr>
2024-12-13 09:58:16 +0900

    자동차 테스트 - answer - bisect 함수 특성 이용

--- HSAT_7/car_test/6247_answer.py
+++ HSAT_7/car_test/6247_answer.py
@@ -7,8 +7,8 @@ setMileage = set(mileage) # hash -> 상수시간에 탐색
 
 for _ in range(q):
     m = int(input())
-    if m not in setMileage: # list 에서 있는지 확인하려면 리스트 전체를 다 뒤져야 하는데 너무 길어요 (5만*20만..> 10억)
-        print(0)
-    else:
-        idx = bisect.bisect_left(mileage, m)
+    idx = bisect.bisect_left(mileage, m) # 들어있으면 본인 위치, 들어있지 않으면 삽입될 위치를 나타내는 값이다.
+    if idx != n and m == mileage[idx]:
         print(idx*(n-idx-1)) # 자기보다 작은거 x 자기보다 큰거 개수
+    else: # 없는 값
+        print(0)
